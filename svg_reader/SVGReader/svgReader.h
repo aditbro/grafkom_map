b1f29f20be709922461a407372ead8b95d9616cf
Daniel Y <danielyg2904@gmail.com>
2019-02-26 07:49:20 -0800

    added layering reading on svgreader

--- svg_reader/SVGReader/svgReader.h
+++ svg_reader/SVGReader/svgReader.h
@@ -17,13 +17,17 @@
 char*
 getPathData(xmlTextReaderPtr reader);
 
+
+char* 
+getLayerName(xmlTextReaderPtr reader);
 /**
  * streamFile:
  * @filename: the file name to parse
+ * @outLayerNames: output layer names corresponding to return value
  *
  * Parses SVG and returns an array of string that represents unprocessed path data
  */
 char**
-streamFile(const char *filename);
+streamFile(const char *filename, char** outLayerNames);
 
 #endif
